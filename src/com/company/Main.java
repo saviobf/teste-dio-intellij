e7798c06132a9d45832a7c86b3f6a46fc8bf3e8b
Savio <saviobf@gmail.com>
2021-09-28 14:27:44 -0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import com.company.model.Gato;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        /*int a=10;
+        int b=20;
+        System.out.println("soma = " + (a+b));*/
+    }
+}
